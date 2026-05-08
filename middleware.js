10cfc920ca9532afea78a71009542ec4ad53f84d
franklmcaetano <71623704+franklmcaetano@users.noreply.github.com>
2026-05-07 23:21:14 -0300

    Add authentication middleware with config

--- middleware.js
+++ middleware.js
@@ -1,1 +1,11 @@
+import { withAuth } from "next-auth/middleware";
 
+export default withAuth({
+  callbacks: {
+    authorized: ({ token }) => !!token,
+  },
+});
+
+export const config = {
+  matcher: ["/painel/:path*", "/api/analise/:path*"],
+};
